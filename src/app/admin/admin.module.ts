8adfddb41036a54e9f08143ed7a77115f330b951
yonatanziv <Yoniziv1!>
2017-05-09 17:45:09 +0300

    Created Admin panel - Users list with search filter.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin/admin.component';
 import {MaterialUtilModule} from '../material-util/material-util.module';
+import {PrimeNGUtilModule} from "../prime-ngutil/prime-ngutil.module";
+import { UsersComponent } from './admin/users/users.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialUtilModule
+    MaterialUtilModule,
+    PrimeNGUtilModule,
+    BrowserModule,
+    FormsModule // Not sure if this is ok
   ],
-  declarations: [AdminComponent]
+  declarations: [AdminComponent, UsersComponent],
 })
 export class AdminModule { }
